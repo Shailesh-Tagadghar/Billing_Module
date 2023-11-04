ae24f57119b99a5eebf0f9b05643fbe161a56622
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2023-11-04 12:03:01 +0530

    ok

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const { log } = require('console')
 const express = require('express')
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const connect = require('connect-mongo')
+const exceljs = require('exceljs')
+const mongodb = require('mongodb')
+const pdfkit = require('pdfkit')
+const xlsx = require('xlsx')
+const nodemon = require('nodemon')
+
 const app = express()
-
-app.set('view engine', 'ejs')
-
-
 const port = 3000;
 
 
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', function (req, res) {
-    res.render('/pages/index')
+    res.render('pages/index')
 });
 
 app.get('/', function (req, res) {
